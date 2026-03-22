dbcf6b46eb13b70218b26703979dcc7cd73ca204
Alun 'Will' Williams <alun@alunw.com>
2026-03-22 14:18:56 +0000

    wip

--- src/TransactionsView.jsx
+++ src/TransactionsView.jsx
@@ -106,55 +106,106 @@ export default function TransactionView() {
     currency: 'GBP',
   });
 
+  const currencyFormat = (value) => {
+    if (!value) return value;
+    return new Intl.NumberFormat('en-GB', {
+      style: 'currency',
+      currency: 'GBP',
+    }).format(value);
+  };
+
+  const dateFormat = (value) => {
+    const date = new Date(value);
+    return date.toLocaleDateString('en-GB', {
+      dateStyle: 'short'
+    })
+  };
+
+  const dateTimeFormat = (value) => {
+    const date = new Date(value);
+    return date.toLocaleString('en-GB', {
+      dateStyle: 'short',
+      timeStyle: 'long'
+    })
+  };
+
+  const isoDateFormat = (value) => {
+    const date = new Date(value);
+    return date.toISOString();
+  };
+
   const columns = [
     {
-      field: 'trans_date', headerName: 'Transaction Date', width: 130, type: 'date',
-      valueGetter: (value) => {
-        return new Date(value)
-      }
+      field: 'uuid', cellClassName: 'ro' // TODO 
     },
     {
-      field: 'account', headerName: 'Account', width: 230,
-      valueGetter: (value) => {
-        const acc = accounts.filter((account) => account.uuid === value);
-        // console.log(`Got account: ${JSON.stringify(acc)}`);
-        if (acc.length === 1) {
-          return acc[0].name;
-        } else {
-          return 'ERROR'
-        }
-      }
+      field: 'trans_date', headerName: 'Transaction Date', width: 130, type: 'date', editable: true,
+      valueFormatter: dateFormat
     },
+    { field: 'entry_date', headerName: 'Entry Date', width: 200, valueFormatter: isoDateFormat },
     {
-      field: 'source_type', headerName: 'Src', width: 50,
-      valueGetter: (value) => {
-        return value[0]
-      }
-    },
-    { field: 'description', headerName: 'Description', flex: 1, minWidth: 300 },
-    {
-      field: 'net_amount', headerName: 'Amount', width: 140,
-      type: 'number', editable: true, valueFormatter: (value) => {
-        if (!value) return value;
-        return currencyFormatter.format(value);
+      field: 'account', headerName: 'Account', width: 230, editable: true, type: 'singleSelect',
+      valueOptions: accounts.map((acc) => ({
+        value: acc.uuid,
+        label: acc.name,
+      })),
+      valueFormatter: (value) => {
+        const acc = accounts.find((a) => a.uuid === value);
+        return acc ? acc.name : '';
       },
     },
-    { field: 'account_balance', headerName: 'Account Balance', width: 140 },
     {
-      field: 'category', headerName: 'Category', width: 290,
+      field: 'source_type', headerName: 'Src. Type', width: 50,
       valueGetter: (value) => {
-        const cat = categories.filter((category) => category.uuid === value);
-        // console.log(`Got account: ${JSON.stringify(acc)}`);
-        if (cat.length === 1) {
-          return cat[0].name;
-        } else if (cat.length === 0) {
-          return ''
-        } else {
-          return 'ERROR'
-        }
+        return value[0]; // first character
       }
     },
+    { field: 'source_name', headerName: 'Src. Name', width: 270, },
+    { field: 'source_row', headerName: 'Src. Row', width: 50, type: 'number' },
+    { field: 'type', headerName: 'Type', width: 100, },
+    { field: 'description', headerName: 'Description', flex: 1, minWidth: 320 },
+    { field: 'comment', headerName: 'Comment', flex: 1, minWidth: 200 },
+    {
+      field: 'net_amount', headerName: 'Amount', width: 140,
+      type: 'number', editable: true, valueFormatter: currencyFormat,
+    },
+    { field: 'statement_amount', headerName: 'Stmt. Amount', width: 140, type: 'number', valueFormatter: currencyFormat },
+    { field: 'account_balance', headerName: 'Account Balance', width: 140, type: 'number', valueFormatter: currencyFormat },
+    { field: 'statement_balance', headerName: 'Statement Balance', width: 140, type: 'number', valueFormatter: currencyFormat },
+    {
+      field: 'category', headerName: 'Category', flex: 1, minWidth: 290,
+      valueFormatter: (value) => {
+        const cat = categories.find((a) => a.uuid === value);
+        return cat ? cat.name : '';
+      },
+      // valueGetter: (value) => {
+      //   const cat = categories.filter((category) => category.uuid === value);
+      //   if (cat.length === 1) {
+      //     return cat[0].name;
+      //   } else if (cat.length === 0) {
+      //     return ''
+      //   } else {
+      //     return 'ERROR'
+      //   }
+      // }
+    },
   ];
+
+  const initialState = {
+    columns: {
+      columnVisibilityModel: {
+        uuid: false,
+        entry_date: false,
+        statement_amount: false,
+        statement_balance: false,
+        type: false,
+        comment: false,
+        source_type: false,
+        source_name: false,
+        source_row: false,
+      },
+    },
+  };
 
   return (
     <Box style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column' }}>
@@ -198,11 +249,27 @@ export default function TransactionView() {
         getRowId={(row) => row.uuid}
         density='compact'
         columns={columns}
+        initialState={initialState}
         loading={loading}
+        editMode='row'
         paginationMode="server"
+        // disable both column sorting and filtering
+        // this would need to be handled server-side to be useful
+        disableColumnSorting
+        disableColumnFilter
+        //sortingMode="server"
         paginationModel={paginationModel}
         onPaginationModelChange={setPaginationModel}
         pageSizeOptions={[10, 25, 50]}
+        //sortModel={sortModel}
+        //onSortModelChange={(newModel) => setSortModel(newModel)}
+        sx={{
+          '& .ro': { // read-only className
+            backgroundColor: '#f9f9f9ff', // Light grey background
+            //color: '#818181',           // Muted text color
+            //cursor: 'not-allowed',      // Changes the mouse pointer
+          }
+        }}
       />
     </Box>
   );
